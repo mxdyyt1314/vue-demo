bad91d70d017fd1474d14ff7113c6e2c388432b5
meng <qwertyui>
2016-11-12 17:54:13 +0800

    组件基本用法

--- js/components.js
+++ js/components.js
@@ -1,6 +1,33 @@
-var app1=new Vue({
-    el:"#app-1",
-    data:{
-        "message":"my message is bing"
+Vue.component("my-component", {
+    template: "<div>A customer component!</div>"
+});
+
+var app1 = new Vue({
+    el: "#app-1"
+});
+
+var app2 = new Vue({
+    el: "#app-2",
+    components: {
+        "part-component": {
+            template: "<div>a part component</div>"
+        }
     }
+});
+
+var data = {
+    counter: 1
+};
+
+Vue.component("simple-counter", {
+    template: "<button v-on:click='counter+=1'>{{counter}}</button>",
+    data: function () {
+        return data;
+        // return {
+        //     counter:1
+        // };
+    }
+});
+new Vue({
+    el: "#app-3"
 });
